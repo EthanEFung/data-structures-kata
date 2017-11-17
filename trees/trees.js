c1b90b34d3cc0f8a086442f9c68f531e0959a49d
Ethan Fung <EthanEFung@gmail.com>
2017-11-16 18:45:49 -0800

    feat: add binary depth and breath first search

--- trees/trees.js
+++ trees/trees.js
@@ -30,9 +30,33 @@ function breadthTraverseNonBinaryTree(tree, cb) {
   }
 }
 
+function depthTraverseBinaryTree(tree, cb, firstChild = "left") {
+  let secondChild = firstChild === "left" ? "right" : "left";
+  if (tree[firstChild]) {
+    depthTraverseBinaryTree(tree[firstChild], cb, firstChild);
+  }
+  if (tree[secondChild]) {
+    depthTraverseBinaryTree(tree[secondChild], cb, firstChild);
+  }
+  if (!!tree) cb(tree);
+}
+
+function breadthTraverseBinaryTree(tree, cb, firstChild = "left") {
+  let secondChild = firstChild === "left" ? "right" : "left";
+  const queue = [tree];
+  while (queue.length > 0) {
+    const node = queue.shift();
+    if (node[firstChild]) queue.push(node[firstChild]);
+    if (node[secondChild]) queue.push(node[secondChild]);
+    cb(node);
+  }
+}
+
 module.exports = {
   NonBinaryTree,
   BinaryTree,
   depthTraverseNonBinaryTree,
-  breadthTraverseNonBinaryTree
+  breadthTraverseNonBinaryTree,
+  depthTraverseBinaryTree,
+  breadthTraverseBinaryTree
 };
--- trees/trees.test.js
+++ trees/trees.test.js
@@ -2,16 +2,18 @@ const {
   NonBinaryTree,
   BinaryTree,
   depthTraverseNonBinaryTree,
-  breadthTraverseNonBinaryTree
+  breadthTraverseNonBinaryTree,
+  depthTraverseBinaryTree,
+  breadthTraverseBinaryTree
 } = require("./trees");
-
 const { TestNBT, TestBT, TestBST } = require("./sampleTrees");
 
 const logValue = jest.fn().mockImplementation(tree => console.log(tree.value));
 const pushValue = jest
   .fn()
   .mockImplementation((values, tree) => values.push(tree.value));
-const mockFunctions = [logValue, pushValue];
+const mock = jest.fn().mockImplementation();
+const mockFunctions = [logValue, pushValue, mock];
 
 afterEach(() => {
   mockFunctions.forEach(fn => fn.mockClear());
@@ -46,13 +48,14 @@ describe("Binary Tree", function() {
 
 describe("depthTraverseNonBinaryTree", function() {
   it("should visit all nodes in a tree", function() {
-    depthTraverseNonBinaryTree(TestNBT, logValue);
-    expect(logValue).toHaveBeenCalledTimes(10);
+    depthTraverseNonBinaryTree(TestNBT, mock);
+    expect(mock).toHaveBeenCalledTimes(10);
   });
 
   it("should visit the children prior to the parent", function() {
     const actual = [];
     const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
     depthTraverseNonBinaryTree(TestNBT, t => pushValue(actual, t));
     expect(actual).toEqual(expected);
   });
@@ -60,14 +63,52 @@ describe("depthTraverseNonBinaryTree", function() {
 
 describe("breadth first non-binary tree search", function() {
   it("should visit all nodes in the tree", function() {
-    breadthTraverseNonBinaryTree(TestNBT, logValue);
-    expect(logValue).toHaveBeenCalledTimes(10);
+    breadthTraverseNonBinaryTree(TestNBT, mock);
+    expect(mock).toHaveBeenCalledTimes(10);
   });
   it("should visit the parent prior to the children", function() {
     const actual = [];
     const expected = [10, 4, 5, 6, 7, 8, 9, 1, 2, 3];
 
     breadthTraverseNonBinaryTree(TestNBT, t => pushValue(actual, t));
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("depth first binary tree search", function() {
+  it("should visit every node", function() {
+    depthTraverseBinaryTree(TestBT, mock);
+    expect(mock).toHaveBeenCalledTimes(7);
+  });
+  it("should start with the left side if the firstChild is not specified", function() {
+    const actual = [];
+    const expected = [8, 5, 7, 3, 2, 5, 1];
+    depthTraverseBinaryTree(TestBT, t => pushValue(actual, t));
+    expect(actual).toEqual(expected);
+  });
+  it("should start with the right if specified", function() {
+    const actual = [];
+    const expected = [2, 3, 5, 5, 8, 7, 1];
+    depthTraverseBinaryTree(TestBT, t => pushValue(actual, t), "right");
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("breadth first binary tree search", function() {
+  it("should visit every node", function() {
+    breadthTraverseBinaryTree(TestBT, mock);
+    expect(mock).toHaveBeenCalledTimes(7);
+  });
+  it("should visit parent nodes prior to children", function() {
+    const actual = [];
+    const expected = [1, 7, 5, 8, 5, 3, 2];
+    breadthTraverseBinaryTree(TestBT, t => pushValue(actual, t));
+    expect(actual).toEqual(expected);
+  });
+  it("should visit right node prior to left if specified", function() {
+    const actual = [];
+    const expected = [1, 5, 7, 2, 3, 5, 8];
+    breadthTraverseBinaryTree(TestBT, t => pushValue(actual, t), "right");
     expect(actual).toEqual(expected);
   });
 });
